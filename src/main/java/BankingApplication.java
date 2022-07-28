cc4d8be1e72e64533e35eb6051e115208fa6ddc0
Ian Allender <ianallender@gmail.com>
2022-07-28 17:48:47 -0500

    initial banking application push

---
+++ src/main/java/BankingApplication.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BankingApplication {
+
+    public static void main(String[] args) {
+        BankAccount obj1 = new BankAccount("aaa", "001");
+        obj1.showMenu();
+    }
+}
